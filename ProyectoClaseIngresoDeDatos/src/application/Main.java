bfe88ea74749d081510b4aa0d67e219cfcb57c4a
Feat <jherrera892016@gmail.com>
2020-06-12 19:51:31 -0500

    COMMIT PRUEBA
    Este es un Commit de Prueba

--- ProyectoClaseIngresoDeDatos/src/application/Main.java
+++ ProyectoClaseIngresoDeDatos/src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 //		int menor = obtenerNumeroMenorDeTresNumeros(5, 6, 3);
 //		mostrarAlerta("Hola.. El Numero Menor De " + 5 + " " + 6 + " " + 3 + " Es: " + menor,
 //				"Metodo Numero Menor De Tres", "Cabecera", AlertType.INFORMATION);
-
+		System.out.println("Hola....");
 		int opcion = Integer.parseInt(ingresarMensaje("Opcion", "Menu",
 				"Ingrese 1. Para Verificar Cual Es El Numero Menor\nIngrese 2. Para Verificar Cual Es El Numero Mayor"));
 
